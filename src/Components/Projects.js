e9747ab78e748b4b3699382bbecd4ab0157a0260
hemapriyavk <hemapriya2000mdu@gmail.com>
2024-10-07 12:48:55 +0530

    HP cloth store project added

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -4,12 +4,21 @@ import React from 'react';
 
 const projects = [
   {
+    title: "HP Cloth Store",
+    description: "Integrated user authentication, product management, and secure payment systems.",
+    image: "https://raw.githubusercontent.com/hemapriyavk/E-Commerce-website-using-MERN-Stack/refs/heads/main/HP%20Cloth%20store.png",
+    video: "",
+    github: "https://github.com/hemapriyavk/E-Commerce-website-using-MERN-Stack",
+    tools: ["MERN Stack", "Tailwind CSS"],
+    demo: "https://e-commerce-website-frontend-silk.vercel.app/"
+  },
+  {
     title: "HP Blogs",
     description: "CRUD functionality for blog posts, along with pagination and search capabilities",
     image: "https://raw.githubusercontent.com/hemapriyavk/Blog_application_using_MERN_Stack/refs/heads/master/Output.png",
     video: "https://drive.google.com/file/d/1u6mmVosVeR2mfciChYU1yGj_6_uYb480/view?usp=sharing",
     github: "https://github.com/hemapriyavk/Blog_application_using_MERN_Stack.git",
-    tools: ["MERN Stack", "Bootstrap", "Axios"],
+    tools: ["MERN Stack", "Bootstrap"],
   },
   {
     title: "HP Chatbot",
