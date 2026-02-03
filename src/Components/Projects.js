020eef197f6298374d2e73ed1ee9e33a65695154
Hemapriya <hemapriya@wealwin.com>
2026-02-03 18:52:31 +0530

    Full stack blog application using Next.js project added

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -22,13 +22,13 @@ const projects = [
     demo: "https://food-delivery-app-7g1e.onrender.com/"
   },
   {
-    title: "HP Blogs",
-    description: "CRUD functionality for blog posts, along with pagination and search capabilities",
-    image: "https://raw.githubusercontent.com/hemapriyavk/Blog_application_using_MERN_Stack/refs/heads/master/HP%20blogs.png",
+    title: "HP Bloggers",
+    description: "Featuring blog pages, dynamic routes & admin dashboard powered by API routes",
+    image: "https://raw.githubusercontent.com/hemapriyavk/Full-stack-blog-application-using-Next.js/refs/heads/master/HP_Blogger.png",
     video: "https://drive.google.com/file/d/1d7jN6VGtRfIclmkJ-QqSS37edYW1_1f9/view?usp=sharing",
-    github: "https://github.com/hemapriyavk/Blog_application_using_MERN_Stack.git",
-    tools: ["MERN Stack", "Bootstrap"],
-    demo: "https://hp-blogs.vercel.app/"
+    github: "https://github.com/hemapriyavk/Full-stack-blog-application-using-Next.js",
+    tools: ["Next.js", "MongoDB"],
+    demo: "https://full-stack-blog-application-using-n.vercel.app/"
   },
   {
     title: "Brainwave",
@@ -56,27 +56,36 @@ const projects = [
     github: "https://github.com/hemapriyavk/3d-web-dev-design-company-sample-project",
     tools: ["Three.js", "React.js"],
     demo: "https://3d-web-dev-design-company-sample-project.vercel.app/"
-  },{
+  }, {
     title: "3D Website - Sample Project",
     description: "3D model created using Blender 3D",
     image: "https://raw.githubusercontent.com/hemapriyavk/3d-website-room-sample-project/refs/heads/master/3D%20Room.png",
     video: "https://drive.google.com/file/d/1hi6cb1BqHvV5tO7DiAkErJsDdQhKfxeU/view?usp=sharing",
     github: "https://github.com/hemapriyavk/3d-website-room-sample-project",
     tools: ["Blender", "Three.js"],
-  },{
+  }, {
     title: "HP Chatbot",
     description: "Virtual Assistant - ability to handle basic conversational tasks",
     image: "https://raw.githubusercontent.com/hemapriyavk/Simple-chatbot-using-JavaScript/master/HP_Chatbot.png",
     video: "https://drive.google.com/file/d/1DYtVQC8oULquMBLuUQ9oLSLH-AWkefUW/view?usp=sharing",
     github: "https://github.com/hemapriyavk/Simple-chatbot-using-JavaScript",
     tools: ["JavaScript", "HTML", "CSS"],
-  },{
+  }, {
     title: "Registration Form",
     description: "Register & Login form is a MERN stack project with user authentication.",
     image: "https://raw.githubusercontent.com/hemapriyavk/Register_Login_Form-using-MERN-Stack/master/Register_login_Form.png",
     video: "https://drive.google.com/file/d/1HJG5YZ_OoXsB6vpu1I5hamNhBf-FYojG/view?usp=sharing",
     github: "https://github.com/hemapriyavk/Register_Login_Form-using-MERN-Stack",
     tools: ["MERN Stack", "Bootstrap"],
+  },
+  {
+    title: "HP Blogs",
+    description: "CRUD functionality for blog posts, along with pagination and search capabilities",
+    image: "https://raw.githubusercontent.com/hemapriyavk/Blog_application_using_MERN_Stack/refs/heads/master/HP%20blogs.png",
+    video: "https://drive.google.com/file/d/1d7jN6VGtRfIclmkJ-QqSS37edYW1_1f9/view?usp=sharing",
+    github: "https://github.com/hemapriyavk/Blog_application_using_MERN_Stack.git",
+    tools: ["MERN Stack", "Bootstrap"],
+    demo: "https://hp-blogs.vercel.app/"
   },
   // Add more projects here
 ];
